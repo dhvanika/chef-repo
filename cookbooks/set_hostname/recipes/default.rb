e5c2f9011215f20ee2b6d94f0f5774162f8d4d9b
Eric Busboom <eric@clarinova.com>
2012-10-01 13:15:51 -0700

    ?

--- cookbooks/set_hostname/recipes/default.rb
+++ cookbooks/set_hostname/recipes/default.rb
@@ -21,11 +21,23 @@ bash "update_hosts" do
   EOH
 end
 
+
+template "/etc/hosts" do
+  user 'root'
+  group 'root'
+  mode '0644'
+  variables(
+      :hostname => node.set_hostname.hostname # Not used
+  )
+  source "hosts.erb"
+end
+
+# Replace the line with this hostname with a local entry
 ruby_block "edit /etc/hosts" do
   block do
     file = Chef::Util::FileEdit.new("/etc/hosts")
      # the 127.0.1.1 address is particular to vagrant; need to do and if/else for final
-    file.search_file_replace_line(/127.0.1.1/, "127.0.1.1 #{node.set_hostname.hostname}")
+    file.search_file_replace_line(/#{Regexp.quote(node.set_hostname.hostname)}/, "127.0.1.1 #{node.set_hostname.hostname}")
     file.write_file
   end
   action :create
